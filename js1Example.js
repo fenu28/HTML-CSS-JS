d472c106d939d617d79307bc2b12c74939a80627
fenu28 <kamdarfenil88@gmail.com>
2024-05-06 20:52:43 +0530

    Initial Commit

---
+++ js1Example.js
@@ -0,0 +1,16 @@
+function changeParagraph() {
+    //document.getElementById("para").innerHTML='Paragraph Updated'
+    //document.getElementById("para").innerHTML=5+6
+    let name = "Mercedes"
+    document.getElementById("para").innerHTML=name;
+
+}
+
+function documentWrite(){
+    document.write(5+"6")
+}
+
+function alertExample(){
+    window.alert("This is an example of alert")
+}
+
